db3e31079015879d39f18f24e0295b387a40e73a
Helo <62700189+helo1313@users.noreply.github.com>
2023-12-07 22:42:55 +0100

    added server site props

--- src/pages/[city].tsx
+++ src/pages/[city].tsx
@@ -1,6 +1,7 @@
 import SearchForm from "@/components/searchForm/SearchForm";
 import WeatherCard from "@/components/weatherCard/WeatherCard";
 import WeatherData from "@/utils/interfaces/WeatherData";
+import { GetServerSideProps } from "next";
 
 const WEATHER_DOMMY_DATA: WeatherData = {
   name: "PARIS",
@@ -10,13 +11,39 @@ const WEATHER_DOMMY_DATA: WeatherData = {
   weather: "clear",
 };
 
-const CityPage: React.FC = () => {
+const CityPage: React.FC = (props: WeatherData) => {
   return (
     <>
       <SearchForm />
-      <WeatherCard {...WEATHER_DOMMY_DATA} />
+      <WeatherCard {...props} />
     </>
   );
 };
 
 export default CityPage;
+
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch(`https://.../data`)
+//   const data = await res.json()
+
+//   // Pass data to the page via props
+//   return { props: { data } }
+// }
+
+export const getServerSideProps: GetServerSideProps<WeatherData> = async (
+  ctx
+) => {
+  const { params } = ctx;
+  const city = params?.city as string;
+
+  return {
+    props: {
+      name: "TESTOWE",
+      temperature: "24",
+      humidity: "13",
+      windSpeed: "10",
+      weather: "clear",
+    },
+  };
+};
